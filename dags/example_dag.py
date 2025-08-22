742586ef8629522c11670797762f1b719361f036
kagansoysal <94367707+kagansoysal@users.noreply.github.com>
2025-08-22 14:59:12 +0300

    Update example_dag.py

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -122,7 +122,7 @@ def insert_to_postgres_logic(df_json):
 
 @dag(
     dag_id="btc_technical_indicators",
-    schedule_interval="*/5 * * * *",
+    schedule="*/5 * * * *",
     start_date=datetime(2025, 1, 1),
     catchup=False,
     default_args=default_args,
